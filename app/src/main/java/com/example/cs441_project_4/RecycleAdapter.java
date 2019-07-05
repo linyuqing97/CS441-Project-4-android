d43ab31e5ee0507fc5f6ec832fae453e43e62868
j <j@DESKTOP-M0RSPA0>
2019-07-04 22:12:37 -0400

    add images recycleViews

---
+++ app/src/main/java/com/example/cs441_project_4/RecycleAdapter.java
@@ -0,0 +1,49 @@
+package com.example.cs441_project_4;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.ImageViewHolder> {
+    private int[] imageArray;
+    public RecycleAdapter(int[] imageArray){
+        this.imageArray = imageArray;
+    }
+
+    @Override
+    public ImageViewHolder  onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.image_layout,parent,false);
+        ImageViewHolder imageViewHolder = new ImageViewHolder(view);
+        return imageViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ImageViewHolder holder, int position) {
+        int image_id = imageArray[position];
+        holder.Images.setImageResource(image_id);
+        holder.TV.setText("Image");
+    }
+
+    @Override
+    public int getItemCount() {
+        return imageArray.length;
+    }
+    public static class ImageViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView Images;
+        TextView TV;
+        public ImageViewHolder(View itemView) {
+            super(itemView);
+            Images= itemView.findViewById(R.id.foodImages);
+            TV = itemView.findViewById(R.id.foodTV);
+
+        }
+    }
+
+}
--- app/src/main/java/com/example/cs441_project_4/TableActivity.java
+++ app/src/main/java/com/example/cs441_project_4/TableActivity.java
@@ -1,38 +1,46 @@
 package com.example.cs441_project_4;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.widget.TextView;
 import android.os.Bundle;
 
 import java.util.ArrayList;
 
-public class TableActivity extends AppCompatActivity {
+public class TableActivity extends AppCompatActivity{
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+
+    private int[] images = {R.drawable.gongbaojiding,R.drawable.mapodoufu,R.drawable.qingzhengluyu,R.drawable.shuizhuniurou};
+  
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_table);
 
-        entries = new ArrayList<>();
-        entries.add("First");
-        entries.add("Second");
-        entries.add("Third");
-        for (int i = 4; i < 50; i = i + 1)
-        {
-            entries.add("Row " + i);
-        }
+        recyclerView = findViewById(R.id.recycle);
+        layoutManager = new GridLayoutManager(this,2);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+        mAdapter = new RecycleAdapter(images);
+        recyclerView.setAdapter(mAdapter);
 
 
-        RecyclerView recyclerView =findViewById(R.id.recycle);
-        adapter = new RecyclerAdapter(this, entries);
-        adapter.setClickListener(this);
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.refreshDrawableState();
+
 
     }
+
+
+
 }

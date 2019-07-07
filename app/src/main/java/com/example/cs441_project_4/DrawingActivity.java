cfc7c0b3c219bcc43e62f0fd3f5c9b96d0f1e44c
j <j@DESKTOP-M0RSPA0>
2019-07-07 18:40:23 -0400

    draw line base on changing m and b

--- app/src/main/java/com/example/cs441_project_4/DrawingActivity.java
+++ app/src/main/java/com/example/cs441_project_4/DrawingActivity.java
@@ -2,40 +2,96 @@ package com.example.cs441_project_4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
+
 import android.os.Bundle;
+
+import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+
 import android.widget.LinearLayout;
 import android.widget.SeekBar;
-import android.graphics.drawable.Drawable;
-import android.view.View;
-import android.content.Context;
-import android.graphics.Canvas;
+
 
 public class DrawingActivity extends AppCompatActivity {
 
+
     MyCanvas myCanvas;
+    float m=0,b = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_drawing);
+        setContentView(R.layout.activity_drawing);
 
-//        SeekBar seekBarB = (SeekBar)findViewById(R.id.seekBarB);
-//        SeekBar seekBarM = (SeekBar)findViewById(R.id.seekBarM);
-//        Button button = (Button)findViewById(R.id.buttton);
-//        LinearLayout layout = (LinearLayout)findViewById(R.id.customViewLayout);
+        SeekBar seekBarB = (SeekBar)findViewById(R.id.seekBarB);
+
+        Button button = (Button)findViewById(R.id.buttton);
+
+        LinearLayout layout = (LinearLayout)findViewById(R.id.customViewLayout);
+        myCanvas = new MyCanvas(getApplicationContext());
+        layout.addView(myCanvas);
+
+        SeekBar seekBarM = (SeekBar)findViewById(R.id.seekBarM);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                    myCanvas.firstDraw();
+            }
+        });
 
 
-        myCanvas = new MyCanvas(this);
-        myCanvas.setBackgroundColor(Color.BLACK);
-        setContentView(myCanvas);
 
-        ;
+        seekBarM.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int mChange,boolean fromUser) {
+                // TODO Auto-generated method stub
+                    m=mChange;
+
+                    myCanvas.setStopY(m*(myCanvas.getStopX())+b);
+
+                   // myCanvas.setStarY(myCanvas.getStarX()+b);
+                    System.out.println("m:"+m+ " b"+b+ "StarY: "+myCanvas.getStarY()+" StopY"+myCanvas.getStopY());
+                    myCanvas.draw();
+            }
 
 
 
+        });
+        seekBarB.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int bChang,boolean fromUser) {
+                // TODO Auto-generated method stub
+                b=bChang;
+                myCanvas.setStopY(m*myCanvas.getStopX()+b);
+                myCanvas.setStarY(m*(myCanvas.getStarX())+b);
+
+                myCanvas.draw();
+            }
+
+
+
+        });
 
     }
 }
--- app/src/main/java/com/example/cs441_project_4/MyCanvas.java
+++ app/src/main/java/com/example/cs441_project_4/MyCanvas.java
@@ -8,31 +8,20 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 
+import java.util.jar.Attributes;
+
 
 public class MyCanvas extends SurfaceView implements SurfaceHolder.Callback
 {
-    public SurfaceHolder surfaceHolder = null;
-    private Paint paint = null;
-    private float x, y;
+    public SurfaceHolder surfaceHolder;
+    private Paint paint;
+    public float starX =500,starY=500,stopX=900,stopY=900;
 
     public MyCanvas(Context context) {
         super(context);
         paint = new Paint();
         paint.setColor(Color.RED);
     }
-
-
-    public void draw(){
-        surfaceHolder = getHolder();
-
-        Canvas canvas = surfaceHolder.lockCanvas();
-        Paint surfaceBackgound = new Paint();
-        surfaceBackgound.setColor(Color.BLACK);
-        canvas.drawLine(20,20,20,20,paint);
-        paint.setColor(Color.RED);
-        surfaceHolder.unlockCanvasAndPost(canvas);
-    }
-
 
     @Override
     public void surfaceCreated(SurfaceHolder surfaceHolder) {
@@ -47,5 +36,65 @@ public class MyCanvas extends SurfaceView implements SurfaceHolder.Callback
     @Override
     public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
         paint = null;
+    }
+    public void firstDraw(){
+
+        surfaceHolder = getHolder();
+        Canvas canvas = surfaceHolder.lockCanvas();
+
+        Paint surfaceBackground = new Paint();
+
+        surfaceBackground.setColor(Color.BLACK);
+
+        canvas.drawRect(0, 0, this.getWidth(), this.getHeight(), surfaceBackground);
+
+        // Draw the circle.
+        paint.setColor(Color.RED);
+        paint.setStrokeWidth(50);
+        canvas.drawLine(0,0,100,100,paint);
+        // canvas.drawLine(starX,starY,stopX,stopY,paint);
+        // Unlock the canvas object and post the new draw.
+        surfaceHolder.unlockCanvasAndPost(canvas);
+    }
+
+
+    public void draw(){
+
+        surfaceHolder = getHolder();
+
+        // Get and lock canvas object from surfaceHolder.
+        Canvas canvas = surfaceHolder.lockCanvas();
+
+        Paint surfaceBackground = new Paint();
+
+        surfaceBackground.setColor(Color.BLACK);
+
+        canvas.drawRect(0, 0, this.getWidth(), this.getHeight(), surfaceBackground);
+
+        // Draw the circle.
+        paint.setColor(Color.RED);
+        paint.setStrokeWidth(50);
+            //canvas.drawLine(500,500,2000,2000,paint);
+        canvas.drawLine(starX,starY,stopX,stopY,paint);
+        // Unlock the canvas object and post the new draw.
+        surfaceHolder.unlockCanvasAndPost(canvas);
+
+
+    }
+    public float getStarX(){return this.starX;};
+    public float getStarY(){return this.starY;};
+    public float getStopX(){return this.stopX;};
+    public float getStopY(){return this.stopY;};
+
+
+    public void setStarX(float x) { this.starX = x; }
+    public void setStarY(float y) {
+        this.starY = y;
+    }
+    public void setStopX(float stopX) {
+        this.stopX = stopX;
+    }
+    public void setStopY(float stopY) {
+        this.stopY = stopY;
     }
 }
